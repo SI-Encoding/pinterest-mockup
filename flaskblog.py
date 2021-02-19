672bcc03b6fa6603638ab979c691a876b1f36277
sisik <sisik@sfu.ca>
2021-02-18 17:23:59 -0800

    added login page and trying to implement uploading images

--- flaskblog.py
+++ flaskblog.py
@@ -1,55 +1,142 @@
 ### Example inspired by Tutorial at https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
 ### However the actual example uses sqlalchemy which uses Object Relational Mapper, which are not covered in this course. I have instead used natural sQL queries for this demo. 
 
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm, BlogForm, LoginForm
+from werkzeug.utils import secure_filename
+from flask_wtf.file import FileField, FileAllowed
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+import os
+import secrets
 
-#this is where the data for the posts are stored.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'eca8a83f56f05a17a27d8076f0904b1f'
+
+photos = UploadSet("photos", IMAGES)
+app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
+configure_uploads(app, photos)
+
+#May delete
+UPLOAD_FOLDER = '../static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 posts = [
-  {
-      'username': 'James',
-      'title': 'How to Build a Data Science Portfolio',
-      'content': 'The best way to build a data science portfolio is to do a project.',
-  },
-  {
-      'username': 'Jane',
-      'title': 'Blockchain Could Unlock Vital Funding to Tackle Climate Change',
-      'content': 'Billions of dollars in promised funding is failing to reach the world’s poorest countries — but technologists have a fix in mind .....',
-  }, 
     {
-    'username': 'Jack',
-    'title': 'Can Data Save the Great Barrier Reef?',
-    'content': 'Marine scientists are using technology to track the overall health of the reef.'
+        'username': 'James',
+        'title': 'How to Build a Data Science Portfolio',
+        'content': 'The best way to build a data science portfolio is to do a project',
+    },
+    {
+        'username': 'Jane',
+        'title': 'Blockchain Could Unlock Vital Funding to Tackle Climate Chang',
+        'content': 'Billions of dollars in promised funding is failing to reach the world’s poorest countries — but technologists have a fix in mind .....',
     }
 ]
+users = [(0, 'James'), (1, 'Jane')]
 
-
-#try adding the following code above and refresing your browser (be sure to check your formatting)
-"""
-, 
-{
-  'username': 'Jack',
-  'title': 'Can Data Save the Great Barrier Reef?',
-  'content': 'Marine scientists are using technology to track the overall health of the reef.'
-}
-  
-"""
-
-#routing code (displays each page)
+#Main Page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+#Add Image
+@app.route("/upload")
+def upload():
+    return render_template('upload.html', posts=posts)
 
+
+#Add User
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        users.append((len(users), form.username.data))
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
+#Sign In
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have logged in','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email address and password', 'danger') 
+    return render_template('login.html', title='Login', form=form)
+
+#Add Blog
 @app.route("/blog", methods=['GET', 'POST'])
 def blog():
-    return render_template('blog.html', title='Blog')
+ 
+        
+    
+ 
+    #Blog
+    form = BlogForm()
+    form.username.choices = users
+    if form.validate_on_submit():
+        choices = form.username.choices
+        user =  (choices[form.username.data][1])
+        title = form.title.data
+        #image
+        photos.save(request.files['photo'])
+
+        content = form.content.data
+        posts.insert(0, {
+            'username': user,
+            'title': title,
+            'content': content,
+            'photos' : photos
+        })
+        
+        flash(f'Blog created for {user}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('blog.html', title='Blog', form=form)
+
+
+#Save user profilepic
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+#User Account
+@app.route('/account', methods=['GET','POST'])
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.photo.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file    
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data =current_user.username
+        form.email.data =current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file = image_file, form=form)    
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import  FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_login import current_user
+
+#Form for making a new user
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) 
+    email = StringField('Email',validators=[DataRequired(), Email()]) 
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+#Login for user
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(), Email()]) 
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+#Form for making a new blog
+class BlogForm(FlaskForm):
+    username = SelectField('Username', choices=[], coerce=int)
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    photos = FileField("Please select an image to upload", validators=[FileRequired()])
+    submit = SubmitField('Submit')
+
+#Updating account form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) 
+    email = StringField('Email',validators=[DataRequired(), Email()]) 
+    photo = FileField(' Update Profile Pic', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
