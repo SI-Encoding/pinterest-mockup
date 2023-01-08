f8a780ee5d54af9e72be956250452134c542c65f
SI-Encoding <stanscoding12+encoding@gmail.com>
2023-01-08 12:49:19 -0800

    Reconfigured configuration file.

--- private/config.php
+++ private/config.php
@@ -5,11 +5,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'database');
 
-$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die('Connection failed');
 
-// Check connection
-// if($link === false){
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
+
 
 ?>
